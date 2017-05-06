6b96b4c919ef9bb10c64b05b7ee0b3f439725e31
Mike <mprquinn@gmail.com>
2017-05-06 12:02:00 -0600

    start layout done

--- js/site.js
+++ js/site.js
@@ -1,37 +1,24 @@
 ;(function () {
 	const menuToggle = document.querySelector('.menu__toggle'),
 	  intro = document.querySelector('.intro'),
-	  menu = document.querySelector('.menu'),
-	  divider = document.querySelector('.intro__divider');
+	  menu = document.querySelector('.menu')
 
-	let navToggled = false,
-		color = 'black';
+	let navToggled = false;
 
 	window.addEventListener('load', () => {
 		initSite();
-		dividerSize();
 	});
-	window.addEventListener('resize', dividerSize);
+
 	menuToggle.addEventListener('click', toggleNav);
 
 	function initSite() {
 		intro.classList.add('intro--loaded');
 	}
 
-	function dividerSize() {
-		let tW = document.querySelector('.intro__name').offsetWidth;
-		divider.style.width = `${tW}px`;
-		console.log(tW);
-	}
 
 	function toggleNav() {
 		toggleNav = !toggleNav;
 		menu.classList.toggle('menu--opened');
 		menuToggle.classList.toggle('menu--opened');
-		if (color === 'black') {
-			color = 'white';
-		} else {
-			color = 'black';
-		}
 	}
 })();
