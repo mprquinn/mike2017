8323cbc836e17bd1f6adc26a26549f400e88eb12
Mike <mprquinn@gmail.com>
2017-05-13 09:11:27 -0600

    lol this is gonna blow up computers

--- js/generateBGs.js
+++ js/generateBGs.js
@@ -6,26 +6,40 @@
 	function generateBGs (_this) {
 		const innerImage = _this.children,
 					shapes = ['circle', 'zigzag'],
-					color = '#efefef',
+					color = '#111111',
 					boundaries = _this.getBoundingClientRect(),
 					topOffset = _this.offsetTop,
-					minOffset = 20;
+					minOffset = 60;
 
 		let lastPos,
-				shapeC = 20;
+				shapeC = 20,
+				lastY = 0;
 
-		console.log(topOffset);
+		for (y = topOffset+minOffset; y < topOffset + (boundaries.bottom - boundaries.top) -minOffset; y+=minOffset) {
+			const push = (lastY%2) !== 0 ? minOffset*1.5 : minOffset;
+
+			lastY++;
+			for (x = push; x < (boundaries.right - boundaries.left) -push; x+=minOffset) {
+
+				const doodle = new mojs.Shape({
+					shape: shapes[0],
+					stroke: color,
+					strokeWidth: {1 : 3},
+					radius: {2.5 : 5},
+					opacity: {0.3 : 1},
+					fill: 'none',
+					isShowStart: true,
+					left: x,
+					top: y,
+					isYoyo: true,
+					repeat: 99999,
+					delay: Math.random() * 500,
+				}).play();
+
+				doodle.el.style.zIndex = 0;
+			}
+		}
 		
-		const testShape = new mojs.Shape({
-			shape: shapes[0],
-			stroke: color,
-			strokeWidth: 3,
-			radius: 5,
-			fill: 'none',
-			isShowStart: true,
-			left: boundaries.left + 10,
-			top: topOffset + 10
-		});
 	}
 
 })();
